c14c10c38f6e1294a92120b36c47954cc29c4e99
Lifepillar <lifepillar@lifepillar.me>
2017-07-17 17:01:43 +0200

    Generate a test file.

--- src/pgsql.sql
+++ src/pgsql.sql
@@ -467,11 +467,50 @@ commit;
 
 -- Generate test file
 
-\o test.vim
-select '-- Extensions';
-select extname from extension_names();
+create or replace function vim_extensions()
+returns setof text
+language plpgsql stable
+set search_path to "public" as
+$$
+declare
+  _ext record;
+begin
+  for _ext in select extname from extension_names() loop
+
+    return query
+    select '-- Extension: ' || _ext.extname;
+
+    return query
+    select regexp_replace(synkeyword, '^\w+\.|"', '', 'g')       ||
+           case when synclass = 'function' then '()' else '' end ||
+           ' -- ' || synclass
+      from get_extension_objects(_ext.extname);
+
+  end loop;
+
+  return;
+end;
+$$;
+
+
+\o test.sql
 select '-- Statements';
 select stm from get_statements();
+select '-- Types';
+select "type" from get_types();
+select 'pg_toast_1234';
+select '-- Built-in functions';
+select synfunction || '()' from get_builtin_functions();
+select vim_extensions();
+select '-- Extensions names';
+select extname from extension_names() where not extname ~* '-';
+select '-- Catalog tables';
+select table_name from get_catalog_tables();
 select '-- Built-in keywords';
-select
-
+select keyword from get_keywords();
+select '-- Additional keywords';
+select keyword from get_additional_keywords();
+select '-- Additional constants';
+select keyword from get_additional_constants();
+select '-- Error codes';
+select errcode from get_errcodes();
